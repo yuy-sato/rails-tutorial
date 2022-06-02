e8879411f8b60631b36cf7f4e24e03e2b38ba6ea
yuya.sato <yuya.sato@livesense.co.jp>
2022-06-02 19:50:35 +0900

    ログイン周りのtestを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,10 @@ FactoryBot.define do
   factory :user do
     name { 'Michael Example' }
     email { 'michael@example.com' }
-    password { 'password' }
-    password_confirmation { 'password' }
+    password_digest { User.digest('password') }
     admin { true }
+    activated { true }
+    activated_at { Time.zone.now }
   end
 
   factory :continuous_users, class: User do
